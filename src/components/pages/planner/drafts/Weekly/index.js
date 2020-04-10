fa10e40fe521a0e4a15671a5f7298ad5b3ac0847
cicomsa <ioanacomsa85@gmail.com>
2020-04-10 16:38:47 +0100

    weekly content

--- src/components/pages/planner/drafts/Weekly/index.js
+++ src/components/pages/planner/drafts/Weekly/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import Link from 'next/link'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  border-bottom: 1px solid orange;
+  border-left: 1px solid orange;
+  display: flex;
+  justify-content: space-around;
+`
+
+const Form = styled.div`
+  border-right: 1px solid orange;
+  text-align: center;
+`
+
+const Textarea = styled.textarea`
+  display: block;
+  height: 100px;
+  margin: auto;
+  width: 95%;
+`
 const Index = () => {
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   return (
-    <div>Something</div>
+    <Container>
+      {days.map(day => {
+        return (
+          <Form key={day}>
+            <p>{day}</p>
+            <Textarea type="text" />
+          </Form>
+        )
+      })}
+    </Container>
   )
 }
 
--- src/components/pages/planner/drafts/index.js
+++ src/components/pages/planner/drafts/index.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import styled from 'styled-components'
 import Weekly from './Weekly'
+
+const Links = styled.div`
+  margin-bottom: 50px;
+`
+const Weeklies = styled.div`
+  border-top: 1px solid orange;
+`
 
 const Index = ({ category }) => {
   return (
-    <div>
-      {category && (
-        <Link href='/planner/drafts/'>
-          <a>Drafts</a>
-        </Link>
+    <>
+      <Links>
+        {category && (
+          <Link href='/planner/drafts/'>
+            <a>Drafts</a>
+          </Link>
+        )}
+        {!/weekly/.test(category) && (
+          <Link href='/planner/drafts/weekly'>
+            <a>Weekly</a>
+          </Link>
+        )}
+      </Links>
+      {/weekly/.test(category) && (
+        <Weeklies>
+          <Weekly />
+          <Weekly />
+          <Weekly />
+          <Weekly />
+        </Weeklies>
       )}
-      {!/weekly/.test(category) && (
-        <Link href='/planner/drafts/weekly'>
-          <a>Weekly</a>
-        </Link>
-      )}
-      {/weekly/.test(category) && <Weekly />}
-    </div>
+    </>
   )
 }
 
