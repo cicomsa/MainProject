0ae751ea3376d6b21e572a8a18c28e1b7c8fa607
cicomsa <ioanacomsa85@gmail.com>
2020-04-17 15:14:34 +0100

    refactored code + comments Editor component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -6,29 +6,11 @@ import { Slate, Editable } from 'slate-react'
 import isHotkey from 'is-hotkey'
 import { HandleChangeContext } from '../pages/planner/drafts/Periods'
 
-const CustomEditor = {
-  isMarkActive(editor, mark) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.type === mark,
-    })
-
-    return !!match
-  },
-
-  toggleMark(editor, mark) {
-    const isActive = CustomEditor.isMarkActive(editor, mark)
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : mark },
-      { match: n => Text.isText(n), split: true }
-    )
-  },
-}
-
+// element marks
 const returnElement = (element, attributes, children) => {
   switch (element.type) {
     case 'code':
-      return <pre {...attributes}>{children}</pre>
+      return <code {...attributes}>{children}</code>
     case 'underline':
       return <u {...attributes}>{children}</u>
     case 'list':
@@ -40,11 +22,44 @@ const returnElement = (element, attributes, children) => {
     case 'paragraph':
       return <p {...attributes}>{children}</p>
     default:
-      // for Leaf component
+      // mandatory fallback for Leaf component
       return <span {...attributes}>{children}</span>
   }
 }
 
+// render elements - default/custom(leaf)
+const renderElement = (type = 'default') => useCallback(({
+  attributes, children, element, leaf // values from props
+}) => {
+  if (type === 'leaf') {
+    return returnElement(leaf, attributes, children)
+  }
+
+  return returnElement(element, attributes, children)
+}, [])
+
+const CustomEditor = {
+  // add new element mark to editor
+  isMarkActive(editor, mark) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === mark,
+    })
+
+    return !!match
+  },
+
+  // create element mark toggle
+  toggleMark(editor, mark) {
+    const isActive = CustomEditor.isMarkActive(editor, mark)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : mark },
+      { match: n => Text.isText(n), split: true }
+    )
+  }
+}
+
+// on key down action - toggle between elements marks
 const onKeyDown = (event, editor) => {
   const isBoldHotkey = isHotkey('mod+b')
   const isItalicHotkey = isHotkey('mod+i')
@@ -68,23 +83,8 @@ const onKeyDown = (event, editor) => {
   }
 }
 
-const Leaf = props => {
-  const { leaf, attributes, children } = props
-  return returnElement(leaf, attributes, children)
-}
-
 const Index = ({ editor, value }) => {
   const handleChange = useContext(HandleChangeContext)
-
-  const renderElement = useCallback(props => {
-    const { attributes, children, element } = props
-    return returnElement(element, attributes, children)
-  }, [])
-
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
-
   const changeValue = value => handleChange(value)
 
   return (
@@ -92,8 +92,8 @@ const Index = ({ editor, value }) => {
       {editor && (
         <Slate editor={editor} value={value} onChange={changeValue}>
           <Editable
-            renderElement={renderElement}
-            renderLeaf={renderLeaf}
+            renderElement={renderElement()}
+            renderLeaf={renderElement('leaf')}
             onKeyDown={event => onKeyDown(event, editor)}
           />
         </Slate>
