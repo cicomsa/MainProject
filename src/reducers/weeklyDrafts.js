5a240a1e9c762715373cbe0c7005f00db80ac88b
cicomsa <ioanacomsa85@gmail.com>
2020-04-18 01:26:53 +0100

    update yarn + return correct value - weeklyDrafts reducer

--- src/reducers/weeklyDrafts.js
+++ src/reducers/weeklyDrafts.js
@@ -1,16 +1,7 @@
 const reducer = (state = [], { type, payload }) => {
   switch (type) {
     case "ADD_COPY":
-      const index = state.findIndex(el => el[0] && el[0].id === payload[0].id)
-
-      if (index !== -1) {
-        return [
-          ...state.slice(0, index),
-          payload
-        ]
-      }
-
-      return [...state, payload]
+      return payload
     default:
       return state;
   }
