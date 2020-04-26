876463f21973d1cb83d8bfc7f21d63924cdecd71
cicomsa <ioanacomsa85@gmail.com>
2020-04-26 23:22:10 +0100

    comment broken code

--- src/components/pages/planner/drafts/Periods/index.js
+++ src/components/pages/planner/drafts/Periods/index.js
@@ -25,13 +25,13 @@ const Index = ({ timePeriod, category, savedValues }) => {
     action({ type: 'REMOVE_PERIOD_DATA', payload: { period: period - 1, editors: editorsList } })
   }
 
-  let timeout = null
-  const saveValues = useCallback(values => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      dispatch(addCopy({ [category]: values }))
-    }, 2000);
-  }, [])
+  // let timeout = null
+  // const saveValues = useCallback(values => {
+  //   clearTimeout(timeout);
+  //   timeout = setTimeout(() => {
+  //     dispatch(addCopy({ [category]: values }))
+  //   }, 2000);
+  // }, [])
 
   const handleChange = val => {
     const newValues = values.map(vals =>
@@ -42,7 +42,8 @@ const Index = ({ timePeriod, category, savedValues }) => {
     )
 
     action({ type: 'SET_VALUES', payload: newValues })
-    saveValues(newValues)
+    // saveValues(newValues)
+    dispatch(addCopy({ [category]: values }))
   }
 
   return (
