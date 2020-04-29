168b8db7c9d51539ac95e7cc331c3408846effca
cicomsa <ioanacomsa85@gmail.com>
2020-04-29 13:01:34 +0100

    update reducer to remember state correctly based on id when switching between categories

--- src/reducers/drafts.js
+++ src/reducers/drafts.js
@@ -1,6 +1,14 @@
 const reducer = (state = {}, { type, payload }) => {
   switch (type) {
     case "ADD_COPY":
+      const payloadKey = Object.keys(payload)[0]
+      if (Object.keys(state).includes(payloadKey)) {
+        return {
+          ...state,
+          [payloadKey]: { ...state[payloadKey], ...payload[payloadKey] }
+        }
+      }
+
       return { ...state, ...payload }
     default:
       return state;
